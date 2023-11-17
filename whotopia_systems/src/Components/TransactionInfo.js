da0f855dde117dcdaab5dc9d8eb3e111be33b8b5
baharRi <bahar.riazi@hotmail.no>
2023-11-17 17:12:55 +0100

    Update TransactionInfo.js

--- whotopia_systems/src/Components/TransactionInfo.js
+++ whotopia_systems/src/Components/TransactionInfo.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Card, IconInfo24 } from "@dhis2/ui";
+import { Box, Card, IconInfo16, LogoIcon, Help } from "@dhis2/ui";
 
 export function TransactionInfo() {
   const cardStyle = {
@@ -16,11 +16,26 @@ export function TransactionInfo() {
       <Box height="30em" width="50em">
         <Card>
           <div style={cardStyle}>
-            <IconInfo24 />
-            <p>
-              Select a transaction from the left in order to view more
-              transaction details here.
-            </p>
+            <div
+              style={{
+                width: "100px",
+              }}
+            >
+              <LogoIcon />
+            </div>
+            <br />
+            <div
+              style={{
+                display: "flex",
+                "align-items": "end",
+              }}
+            >
+              <IconInfo16 />
+              <Help>
+                Select a transaction from the left in order to view more
+                transaction details here.
+              </Help>
+            </div>
           </div>
         </Card>
       </Box>
